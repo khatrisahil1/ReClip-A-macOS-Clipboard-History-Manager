aa3cb5533a80a4e7aba1da087f9ddd48a702b689
SAHIL KHATRI <120470284+khatrisahil1@users.noreply.github.com>
2025-05-31 20:23:10 +0530

    Update ReClipApp.swift

--- ReClipApp/MaccyApp.swift
+++ ReClipApp/MaccyApp.swift
@@ -1,10 +1,10 @@
 import SwiftUI
 
 @main
-struct MaccyApp: App {
+struct ReClipApp: App {
   @NSApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
 
-  // It's impossible to create sceneless application,
+  // It's impossible to create a sceneless application,
   // so we are hacking this around by creating a menubar
   // scene that is always hidden.
   @State private var hiddenMenu: Bool = false
