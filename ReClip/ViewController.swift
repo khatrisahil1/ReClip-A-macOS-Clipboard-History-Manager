ce5bebd84224bdae27bbfc60f141750535b9b320
SAHIL KHATRI <khatrisahil821@gmail.com>
2025-06-11 21:27:37 +0530

    Initial Commit

---
+++ ReClip/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  ReClip
+//
+//  Created by SAHIL KHATRI on 11/06/25.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
